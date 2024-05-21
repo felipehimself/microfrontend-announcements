5186e875cbbc7721a8e10c2e2511da17bec32d38
Felype Couto <felype_rocha@ecad.org.br>
2024-05-21 20:51:41 -0300

    feat: added logic to handle theme changes

--- src/features/annoucements/routes/index.tsx
+++ src/features/annoucements/routes/index.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Announcements } from './announcments';
 
 export const AnnouncementsRoutes = () => {
   return (
     <Routes>
       <Route path="*" element={<Announcements />} />
-      {/* <Route path="*" element={<Navigate to="/sales" />} /> */}
+      {/* You could have multiples routes here which would have /announcements as the base route */}
     </Routes>
   );
 };
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Fallback } from '@/components/shared/fallback';
 import { ErrorBoundary } from 'react-error-boundary';
 import { FallbackError } from '@/components/shared/fallback-error';
@@ -6,9 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { darkTheme, lightTheme } from '@mfe-lib/styleguide';
 import { FluentProvider } from '@fluentui/react-components';
 
+const themeChannel = new BroadcastChannel('theme');
+
 export const AppProvider = ({ children }: React.PropsWithChildren) => {
+  const [currentTheme, setCurrentTheme] = useState<'light' | 'dark'>('light');
+
+  themeChannel.onmessage = (event) => setCurrentTheme(event.data);
+
   return (
-    <FluentProvider theme={lightTheme}>
+    <FluentProvider theme={currentTheme === 'light' ? lightTheme : darkTheme}>
       <Suspense fallback={<Fallback />}>
         <ErrorBoundary FallbackComponent={FallbackError}>
           <BrowserRouter>{children}</BrowserRouter>
