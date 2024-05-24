0f2090915e80d1662de51586637bc81f00e6804b
Felype Couto <felype_rocha@ecad.org.br>
2024-05-24 17:10:50 -0300

    fix: fixed change theme

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -11,7 +11,9 @@ const themeChannel = new BroadcastChannel('theme');
 export const AppProvider = ({ children }: React.PropsWithChildren) => {
   const [currentTheme, setCurrentTheme] = useState<'light' | 'dark'>('light');
 
-  themeChannel.onmessage = (event) => setCurrentTheme(event.data);
+  themeChannel.onmessage = (event) => {
+    setCurrentTheme(event.data);
+  };
 
   return (
     <FluentProvider theme={currentTheme === 'light' ? lightTheme : darkTheme}>
