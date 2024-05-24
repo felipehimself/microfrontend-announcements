f7fb29187fef9597a8ed22c73bbbfd14e4af4902
Felype Couto <felype_rocha@ecad.org.br>
2024-05-24 19:40:07 -0300

    fix: fixed theme change listening for events

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Fallback } from '@/components/shared/fallback';
 import { ErrorBoundary } from 'react-error-boundary';
 import { FallbackError } from '@/components/shared/fallback-error';
@@ -6,14 +6,28 @@ import { BrowserRouter } from 'react-router-dom';
 import { darkTheme, lightTheme } from '@mfe-lib/styleguide';
 import { FluentProvider } from '@fluentui/react-components';
 
-const themeChannel = new BroadcastChannel('theme');
-
 export const AppProvider = ({ children }: React.PropsWithChildren) => {
   const [currentTheme, setCurrentTheme] = useState<'light' | 'dark'>('light');
 
-  themeChannel.onmessage = (event) => {
-    setCurrentTheme(event.data);
-  };
+  useEffect(() => {
+    const theme = localStorage.getItem('mfe-theme') as 'light' | 'dark';
+
+    if (theme) {
+      setCurrentTheme(theme);
+    }
+
+    window.addEventListener('mfe-theme', (event) => {
+      const customEvent = event as CustomEvent<{ theme: 'light' | 'dark' }>;
+
+      setCurrentTheme(customEvent.detail.theme);
+    });
+
+    return () => {
+      window.removeEventListener('mfe-theme', (event) => {
+        console.log(event);
+      });
+    };
+  }, []);
 
   return (
     <FluentProvider theme={currentTheme === 'light' ? lightTheme : darkTheme}>
